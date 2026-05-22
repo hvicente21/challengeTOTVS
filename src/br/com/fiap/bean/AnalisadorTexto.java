0c4f220218df168764cce5eb01b7ed9758a17afe
hvicente21 <hvicente2006@gmail.com>
2026-05-22 11:36:49 -0300

    Adicionando classe AnalisadorTexto

---
+++ src/br/com/fiap/bean/AnalisadorTexto.java
@@ -0,0 +1,10 @@
+package br.com.fiap.bean;
+
+import java.util.List;
+
+public class AnalisadorTexto {
+    private List<String> palavrasPositivas;
+    private List<String> palavrasNegativas;
+    private List<String> palavrasRisco;
+    private List<String> palavrasOportunidade;
+}
