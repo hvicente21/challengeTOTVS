0673f3b54cdcabd1d59af24a45b9df499ab2c74e
hvicente21 <hvicente2006@gmail.com>
2026-05-26 12:27:14 -0300

    Finalizando classe ProcessadorTexto

---
+++ src/br/com/fiap/bean/ProcessadorTexto.java
@@ -0,0 +1,35 @@
+package br.com.fiap.bean;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ProcessadorTexto {
+    //construtor
+    public ProcessadorTexto() {}
+    //metodos da classe
+    public String removerPontuacao(String texto){
+        if (texto == null || texto.isBlank()){
+            return "";
+        }
+        return texto.replaceAll("[^a-zA-ZÀ-ÿ0-9 ]", "");
+    }
+
+    public String limparTexto(String texto){
+        if (texto == null || texto.isBlank()) {
+            return "";
+        }
+        texto = texto.toLowerCase();
+        texto = removerPontuacao(texto);
+        return texto.trim();
+    }
+
+    public List<String> tokenizarTexto(String texto){
+        texto = limparTexto(texto);
+        return Arrays.asList(texto.split("\\s+"));
+
+    }
+
+
+
+
+}
