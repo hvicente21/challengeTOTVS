f6774f14d8d39659ad563258e4d1202c1dd99757
hvicente21 <hvicente2006@gmail.com>
2026-05-26 21:18:45 -0300

    Criando a classe main

---
+++ src/br/com/fiap/main/Main.java
@@ -0,0 +1,7 @@
+package br.com.fiap.main;
+
+public class Main {
+    public static void main(String[] args) {
+        String texto = "Bom dia, João. Então, o nosso Protheus está atendendo o backoffice, mas o time de RH está sofrendo muito com a folha manual. O pessoal viu uma demo da Senior e gostou, mas eu prefiro consolidar tudo na TOTVS se o RM for realmente integrado. Ah, e por favor, nao mencione esse valor de R$ 50 que conversamos para o meu CFO ainda, ele está focado no ROI do semestre.";
+    }
+}
