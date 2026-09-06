c09b3b8d3ac69038c7ff424a81ee890a6df633a3
hvicente21 <hvicente2006@gmail.com>
2026-09-05 21:41:40 -0300

    Atualizando classe ProcessadorTexto

--- src/br/com/fiap/service/ProcessadorTexto.java
+++ src/br/com/fiap/service/ProcessadorTexto.java
@@ -1,22 +1,59 @@
 package br.com.fiap.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ProcessadorTexto {
 
     public String limparTexto(String texto) {
-        if (texto == null) return "";
+        if (texto == null) {
+            return "";
+        }
         texto = texto.toLowerCase();
-        return removerPontuacao(texto);
+        texto = texto.trim();
+        texto = texto.replaceAll("\\s+", " ");
+        return texto;
     }
 
     public String removerPontuacao(String texto) {
-        return texto.replaceAll("[^a-zA-ZÀ-ÿ0-9 ]", "");
+        if (texto == null) {
+            return "";
+        }
+        texto = texto.replaceAll("[^\\p{L}\\p{N}\\s]", " ");
+        texto = texto.replaceAll("\\s+", " ").trim();
+
+        return texto;
     }
 
     public List<String> tokenizar(String texto) {
         texto = limparTexto(texto);
+        texto = removerPontuacao(texto);
+        if (texto.isEmpty()) {
+            return new ArrayList<>();
+        }
         return Arrays.asList(texto.split("\\s+"));
+    }
+    public List<String> removerStopwords(List<String> palavras) {
+        List<String> palavrasFiltradas = new ArrayList<>();
+        if (palavras == null) {
+            return palavrasFiltradas;
+        }
+        List<String> stopwords = Arrays.asList(
+                "a", "o", "os", "as",
+                "de", "da", "do", "das", "dos",
+                "e", "em", "para", "com",
+                "um", "uma"
+        );
+        for (String palavra : palavras) {
+
+            if (palavra != null && !palavra.trim().isEmpty()) {
+                String palavraNormalizada = palavra.trim().toLowerCase();
+                if (!stopwords.contains(palavraNormalizada)) {
+                    palavrasFiltradas.add(palavraNormalizada);
+                }
+            }
+        }
+        return palavrasFiltradas;
     }
 }
